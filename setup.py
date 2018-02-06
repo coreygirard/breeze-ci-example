7d2a062f59f680dd3ee663962dcd82e4583de3d3
Corey <corey.r.girard@gmail.com>
2018-02-05 21:42:35 -0500

    Starting PyPI integration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='breeze-ci-test-repo',
+      version='0.1',
+      description=' ',
+      url='http://github.com/crgirard/breeze-ci-example',
+      author='Corey Girard',
+      author_email='corey.r.girard@gmail.com',
+      license='MIT',
+      packages=['example'],
+      zip_safe=False)
